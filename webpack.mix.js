8082d0ace806f6f094b0ec12a2a68120f700390f
Björn <me@tentakelfabrik.de>
2021-04-23 23:43:30 +0200

    adding

---
+++ webpack.mix.js
@@ -0,0 +1,46 @@
+const mix = require('laravel-mix')
+const SvgSpritemapPlugin = require('svg-spritemap-webpack-plugin')
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for your application, as well as bundling up your JS files.
+ |
+ */
+
+mix.webpackConfig({
+    plugins: [
+        new SvgSpritemapPlugin('src/icons/*.svg', {
+            output: {
+                filename: 'dist/symbol-defs.svg',
+                chunk: {
+                    keep: true
+                },
+                svgo: {
+                    plugins: [{
+                        removeAttrs: {
+                            attrs: 'fill'
+                        }
+                    }]
+                },
+                svg4everybody: false
+            },
+            sprite: {
+                prefix: 'icon-'
+            }
+        })
+    ]
+})
+
+mix
+    .sass('src/scss/plain-ui.scss', 'dist/plain-ui.css')
+    .copy('src/fonts/*', 'dist/fonts')
+    .options({
+        postCss: [
+            require('postcss-css-variables')()
+        ]
+    })

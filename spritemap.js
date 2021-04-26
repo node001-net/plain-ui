341e91ebc5ee7c9af673afe1325d326d2692e14c
Björn <me@tentakelfabrik.de>
2021-04-27 00:25:25 +0200

    adding

--- spritemap.js
+++ spritemap.js
@@ -1,1 +1,13 @@
-(self.webpackChunkplain_ui=self.webpackChunkplain_ui||[]).push([[355],{256:()=>{}}]);
+(self["webpackChunkplain_ui"] = self["webpackChunkplain_ui"] || []).push([["spritemap"],{
+
+/***/ "?4e0c":
+/*!******************************!*\
+  !*** spritemap-dummy-module ***!
+  \******************************/
+/***/ (() => {
+
+
+
+/***/ })
+
+}]);

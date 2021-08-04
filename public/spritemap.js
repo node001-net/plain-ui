9bdebb699c60cdde6e32d4bee6798992a2e2f8cd
HerrHase <me@herr-hase.wtf>
2021-08-04 10:35:03 +0200

    adding #26

--- public/spritemap.js
+++ public/spritemap.js
@@ -1,1 +1,13 @@
-(self.webpackChunk_tentakelfabrik_plain_ui=self.webpackChunk_tentakelfabrik_plain_ui||[]).push([[355],{256:()=>{}}]);
+(self["webpackChunk_tentakelfabrik_plain_ui"] = self["webpackChunk_tentakelfabrik_plain_ui"] || []).push([["spritemap"],{
+
+/***/ "?4e0c":
+/*!******************************!*\
+  !*** spritemap-dummy-module ***!
+  \******************************/
+/***/ (() => {
+
+
+
+/***/ })
+
+}]);
